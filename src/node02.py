d064446212cb975c74660ee41ba46468e2b5673e
pitzor <petterwendel@hotmail.com>
2013-04-20 13:11:49 +0200

    node02

---
+++ src/node02.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+When logged in to the game system, your browser will have two new cookies set, �devnull_session� and �devnull_apikey�.
+The first one is recreated for each login, but the second one will be the same between sessions.
+Either one can be used as �session� variable when making a call to the API.
+
+Each team can create a party of up to three player characters, who can explore the dungeons.
+To see which characters are in the party (currently none), use the following API call;
+
+/api3/?session=<sessionkey>&command=getparty
+
+To make sure that the server isn�t overloaded during gameplay, create a request dispatcher that squelches requests per second to the
+server. 
+
+Demonstrate both that information can be fetched (using the getparty call) and that the squelch works when you try to send more than
+10 requests per seconds to the server.
+"""
+
+
+import genericwitticism
+
+         
+def submain():
+    api = genericwitticism.Genericwitticism(key="c577eb41-c931-4526-9c52-e9b361eba8c3")
+    
+    api.start()
+    
+    api.get_party(hej)
+     
+     
+     
+     
+     
+     
+     
+     
