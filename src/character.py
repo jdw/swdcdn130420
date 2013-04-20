6a8731906383504399ea4405805d2c00fa8c816e
pitzor <petterwendel@hotmail.com>
2013-04-20 15:59:45 +0200

    getData

--- src/character.py
+++ src/character.py
@@ -16,7 +16,7 @@ Demonstrate both that information can be fetched (using the getparty call) and t
 10 requests per seconds to the server.
 """
 
-
+import json
 
 class Character(object):
     
@@ -33,17 +33,30 @@ class Character(object):
         self._id = Character._nextId
         Character._nextId += 1
         
-    def _setAttribute(self, attribute, value):
+    def _addToAttribute(self, attribute, value):
         if attribute + value <= 18 and attribute + value > 0 and self._allocationPoints >= value:
             attribute += value
             self._allocationPoints -= value
         return attribute
     
+    def _setAttribute(self, attribute, value):
+        print "setting " , attribute , ": " , value
+        print "(self._allocationPoints + attribute): " , (self._allocationPoints + attribute)
+        print "value: " , value
+        print "(self._allocationPoints + attribute) >= value: " , (self._allocationPoints + attribute) >= int(value)
+        
+        
+        if int(value) <= 18 and int(value) > 0 and (self._allocationPoints + attribute) >= int(value):
+            self._allocationPoints += attribute
+            attribute = int(value)
+            self._allocationPoints -= int(value)
+        return attribute
+    
     def _increaseAttribute(self, attribute):
-        return self._setAttribute(attribute, 1)
+        return self._addToAttribute(attribute, 1)
 
     def _decreaseAttribute(self, attribute):
-        return self._setAttribute(attribute, -1)
+        return self._addToAttribute(attribute, -1)
 
             
     def increaseStrength(self):
@@ -92,23 +105,24 @@ class Character(object):
         return self._wis 
     
     def setStrength(self, value):
-        self._setAttribute(self._str, value)
+        self._addToAttribute(self._str, value)
 
     def setDexterity(self, value):
-        self._setAttribute(self._dex, value)
+        self._addToAttribute(self._dex, value)
 
     def setConstitution(self, value):
-        self._setAttribute(self._con, value)
+        self._addToAttribute(self._con, value)
 
     def setIntelligence(self, value):
-        self._setAttribute(self._int, value)
+        self._addToAttribute(self._int, value)
 
     def setWisdom(self, value):
-        self._setAttribute(self._wis, value)
+        self._addToAttribute(self._wis, value)
 
     @staticmethod
     def test():
         character = Character()
+        character.name = "foobar"
         for i in range(0,8): 
             character.increaseStrength()
         assert character._str == 18
@@ -124,7 +138,36 @@ class Character(object):
         assert character._int == 10
         assert character._wis == 10
         assert character._allocationPoints == 0
+        data = character.__str__()
+        print data
+        character.getData('{"name":"newName","str":"1"}')
+        print character.__str__()
         print "test passed"
+
+        
+    def __str__(self):
+        #'{"key1":1,"key2":2,"key3":3}'
+        #{‘name’:’foobar’,’str’:’15’,’dex’:’15’,’con:’10’,’int’:’10’,’wis’:’10’}
+        #jsobj["a"]["b"]["e"].append({"f":var3, "g":var4, "h":var5})
+        jsonObj = json.dumps({'name': self.name,'str': self._str, 'dex': self._dex, 'con': self._con, 'int': self._int, 'wis': self._wis})
+        return jsonObj.__str__()
+
+    def _setAndValidateData(self,jdata, ObjString, attribute):
+        if ObjString in jdata:
+            if ObjString == "name":
+                self.name = jdata[ObjString]
+            else:
+                return self._setAttribute(attribute, jdata[ObjString])
+        return attribute
+    
+    def getData(self, data):
+        jdata = json.loads(data)
+        self._setAndValidateData(jdata,"name", self.name)
+        self._str = self._setAndValidateData(jdata,"str", self._str)
+        self._dex = self._setAndValidateData(jdata,"dex", self._dex)
+        self._con = self._setAndValidateData(jdata,"con", self._con)
+        self._int = self._setAndValidateData(jdata,"int", self._int)
+        self._wis = self._setAndValidateData(jdata,"wis", self._wis) 
          
 if __name__ == '__main__':
     Character.test()
