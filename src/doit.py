5ce07a3aff391be8d966313dd58b9cf132f6abdb
Johannes D. Wirde <johannes.wirde@gmail.com>
2013-04-20 18:13:35 +0200

    GUI stuff

--- src/doit.py
+++ src/doit.py
@@ -9,6 +9,28 @@ Created on 20 apr 2013
 import node01
 import node02
 import settings
+from threading import Thread
+import time
 
+global running
+def start_gui():
+    global running
+    running = True
+    eval(settings.GUI).start()
+    
+    while running:
+        time.sleep(0.1)    
+        
+    eval(settings.GUI).stop()
+    
 if __name__ == '__main__':
+    #t = Thread(target=start_gui)
+    #t.start()
+    
     eval(settings.NODE).submain()
+    
+    global running
+    running = False
+    
+    
+    
